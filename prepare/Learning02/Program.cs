ef30af2db8c4e18c4e839650d233a912e3d55b54
Your Name <you@example.com>
2023-01-17 17:49:54 -0500

    lesson2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        
+        //call the function "JobInfo()" and pass in the values
+        Job job = new Job();
+        job._companyName = "Microsoft";
+        job._jobTitle = "Software Engineer";
+        job._startYear = 2019;
+        job._endyear = 2022;
+        job.Display();
+
+        Console.WriteLine(); //Leave a space between the ouput.
+
+        Job job1 = new Job();
+        job1._companyName = "Apple";
+        job1._jobTitle = "Data Engineer";
+        job1._startYear =  2011;
+        job1._endyear = 2019;
+        job1.Display();
     }
+}
+
+public class Job 
+{
+    public string _jobTitle = "";
+    public string _companyName = "";
+
+    public int _startYear;
+    
+    public int _endyear;
+
+    public Job()
+    {
+
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_companyName}) {_startYear}-{_endyear}");
+    }
+
+    
+
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
+        Person person = new Person();
+        person._givenName = "Joseph";
+        person._familyName = "Smith";
+        person.ShowWesternName();
+        person.ShowEasternName();
         Console.WriteLine("Hello World!");
+
+        Console.WriteLine();
+        Person person1  = new Person();
+        person1._givenName = "David";
+        person1._familyName = "Adeogun";
+        person1.ShowEasternName();
+        person1.ShowWesternName();
     }
 }
+
+public class Person
+{
+    public string _givenName = "";
+    public string _familyName = "";
+
+    public Person()
+    {
+
+    }
+
+    public void ShowEasternName()
+    {
+        Console.WriteLine($"{_familyName},{_givenName}");
+    }
+
+    public void ShowWesternName()
+    {
+        Console.WriteLine($"{_givenName} {_familyName}");
+    }
+
+
+}
+
